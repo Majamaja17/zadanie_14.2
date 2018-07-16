d2bf08e4938bcd78c81e07dd5422401abb084da1
Maja Jelenik <maja.jelenik@vp.pl>
2018-07-16 03:51:24 +0200

    Change js file

--- script.js
+++ script.js
@@ -3,28 +3,28 @@ var movies = [
     id: 1,
     title: "Ojciec Chrzestny",
     desc: "Totalnie ulubione filmy, ponadczasowe i oddające klimat. Moja ulubiona częśc druga:",
-    image: src="http://1.fwcdn.pl/po/10/90/1090/7196616.3.jpg"
+    image: "http://1.fwcdn.pl/po/10/90/1090/7196616.3.jpg"
   },
   
   {
     id: 2,
     title: "China Town",
     desc: "Film o wartkiej akcji i z młodym Jackiem Nicholsonem :)",
-    image: src="http://1.fwcdn.pl/ph/10/27/1027/426887.1.jpg"
+    image: "http://1.fwcdn.pl/ph/10/27/1027/426887.1.jpg"
   },
 
   {
     id: 3,
     title: "Sekretne okno",
     desc: "Film trzymający z niewiedzy do samego końca",
-    image: src="http://1.fwcdn.pl/po/85/86/98586/6906598.3.jpg"
+    image: "http://1.fwcdn.pl/po/85/86/98586/6906598.3.jpg"
   },
 
   {
     id: 4,
     title: "Crazy, stiupid, love",
     desc: "Komedia, mimo tytułu wcale nie romantyczna zdecydowanie obyczajowa. Wiele Facetów w tajemnicy ją uwielbia:)",
-    image: src="https://is4-ssl.mzstatic.com/image/thumb/Video30/v4/fd/1f/f1/fd1ff191-8578-2fe9-a40c-ac90bd055d53/source/1200x630bb.jpg"
+    image: "https://is4-ssl.mzstatic.com/image/thumb/Video30/v4/fd/1f/f1/fd1ff191-8578-2fe9-a40c-ac90bd055d53/source/1200x630bb.jpg"
   },
 ];
 
